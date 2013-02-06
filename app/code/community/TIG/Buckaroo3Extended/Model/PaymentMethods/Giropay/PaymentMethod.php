a99d025f19742eaa6c5ba3c121e82c9a4af681c6
Total Sandbox <support@totalinternetgroup.nl>
2013-02-06 11:46:54 +0100

    minor changes

--- app/code/community/TIG/Buckaroo3Extended/Model/PaymentMethods/Giropay/PaymentMethod.php
+++ app/code/community/TIG/Buckaroo3Extended/Model/PaymentMethods/Giropay/PaymentMethod.php
@@ -8,4 +8,16 @@ class TIG_Buckaroo3Extended_Model_PaymentMethods_Giropay_PaymentMethod extends T
     protected $_code = 'buckaroo3extended_giropay';
 
     protected $_formBlockType = 'buckaroo3extended/paymentMethods_giropay_checkout_form';
+    
+    public function getOrderPlaceRedirectUrl()
+    {
+        $session = Mage::getSingleton('checkout/session');
+        
+        if(isset($_POST[$this->_code.'_BPE_Bankleitzahl']))
+        {
+            $session->setData('additionalFields', array('Bankleitzahl' => $_POST['buckaroo3extended_giropay_BPE_Bankleitzahl']));
+        }
+        
+        return Mage::getUrl('buckaroo3extended/checkout/checkout', array('_secure' => true, 'method' => $this->_code));
+    }
 }
