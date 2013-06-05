2dce0322a11b7b872c0409bc8dc706346a555ef6
Hendrik Kurniawan <hendrik.kurniawan@totalinternetgroup.nl>
2013-06-05 03:06:47 +0200

    setting amount method into parent class paymentmethod, and implementing account filter

--- app/code/community/TIG/Buckaroo3Extended/Model/PaymentMethods/Paymentguarantee/PaymentMethod.php
+++ app/code/community/TIG/Buckaroo3Extended/Model/PaymentMethods/Paymentguarantee/PaymentMethod.php
@@ -1,26 +1,28 @@
-<?php 
+<?php
 class TIG_Buckaroo3Extended_Model_PaymentMethods_Paymentguarantee_PaymentMethod extends TIG_Buckaroo3Extended_Model_PaymentMethods_PaymentMethod
 {
     public $allowedCurrencies = array(
 		'EUR',
 	);
-	
+
     protected $_code = 'buckaroo3extended_paymentguarantee';
 
     protected $_formBlockType = 'buckaroo3extended/paymentMethods_paymentguarantee_checkout_form';
-    
+
     protected $_canRefund               = false;
     protected $_canRefundInvoicePartial = false;
-    
+
     public function getOrderPlaceRedirectUrl()
     {
         $session = Mage::getSingleton('checkout/session');
-        
+
+        $accountNumber = $_POST[$this->_code.'_bpe_customer_account_number'];
+
         $session->setData(
-        	'additionalFields', 
+        	'additionalFields',
             array(
             	'BPE_Customergender'    => $_POST[$this->_code.'_BPE_Customergender'],
-                'BPE_AccountNumber'     => $_POST[$this->_code.'_bpe_customer_account_number'],
+                'BPE_AccountNumber'     => $this->_validateAccount($accountNumber),
                 'BPE_PhoneNumber'     => $_POST[$this->_code.'_bpe_customer_phone_number'],
         		'BPE_customerbirthdate' => date(
         			'Y-m-d', strtotime($_POST[$this->_code . '_customerbirthdate']['year']
@@ -29,24 +31,24 @@ class TIG_Buckaroo3Extended_Model_PaymentMethods_Paymentguarantee_PaymentMethod 
         		)
         	)
         );
-        
+
     	return parent::getOrderPlaceRedirectUrl();
     }
-    
+
     public function isAvailable($quote = null)
     {
         //check if max amount for Betaalgarant is set and if so, if the quote grandtotal exceeds that
     	$maxAmount = Mage::getStoreConfig('buckaroo/buckaroo3extended_paymentguarantee/max_amount', Mage::app()->getStore()->getStoreId());
     	if (!empty($maxAmount)
-    		&& !empty($quote) 
-    		&& $quote->getGrandTotal() > $maxAmount) 
+    		&& !empty($quote)
+    		&& $quote->getGrandTotal() > $maxAmount)
     	{
     		return false;
     	}
-                  
+
         return parent::isAvailable($quote);
     }
-    
+
     public function validate()
     {
         $postData = Mage::app()->getRequest()->getPost();
@@ -58,9 +60,16 @@ class TIG_Buckaroo3Extended_Model_PaymentMethods_Paymentguarantee_PaymentMethod 
                 Mage::helper('buckaroo3extended')->__('Please accept the terms and conditions.')
             );
         }
-        
+
         $this->getInfoInstance()->setAdditionalInformation('checked_terms_and_conditions', true);
-        
+
         return parent::validate();
+    }
+
+    protected function _validateAccount($accountNumber)
+    {
+        $filteredAccount = str_replace('.', '', $accountNumber);
+
+        return $filteredAccount;
     }
 }
