b39c1472cf3c8c1facbc0b0d59e624fe3dce3c6f
Total Sandbox <support@totalinternetgroup.nl>
2013-09-25 10:44:42 +0200

    test commit

--- app/code/community/TIG/Buckaroo3Extended/Model/Response/Push.php
+++ app/code/community/TIG/Buckaroo3Extended/Model/Response/Push.php
@@ -528,7 +528,8 @@ class TIG_Buckaroo3Extended_Model_Response_Push extends TIG_Buckaroo3Extended_Mo
 	 * @return boolean
 	 */
     protected function _saveInvoice()
-    {  
+    {
+        //test comment  
 	    if ($this->_order->canInvoice()) {
 	        $payment = $this->_order->getPayment();
 	        $payment->registerCaptureNotification($this->_order->getBaseGrandTotal());
