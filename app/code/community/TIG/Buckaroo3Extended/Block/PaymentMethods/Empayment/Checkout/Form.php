6bc65e77971c513f4d0a9386c2d518fc61c904fc
Total Sandbox <support@totalinternetgroup.nl>
2013-11-20 17:15:46 +0100

    empayment bankaccount data is no longer required

--- app/code/community/TIG/Buckaroo3Extended/Block/PaymentMethods/Empayment/Checkout/Form.php
+++ app/code/community/TIG/Buckaroo3Extended/Block/PaymentMethods/Empayment/Checkout/Form.php
@@ -6,14 +6,4 @@ class TIG_Buckaroo3Extended_Block_PaymentMethods_Empayment_Checkout_Form extends
 		$this->setTemplate('buckaroo3extended/empayment/checkout/form.phtml');
         parent::_construct();
     }
-    
-    public function getAccountHolder()
-    {
-    	$accountHolder = $this->getSession()->getData($this->getMethodCode() . '_BPE_Accountholder');
-		
-		if (!$accountHolder) {
-			$accountHolder = $this->getName();
-		}
-        return $accountHolder;
-    }
 }
