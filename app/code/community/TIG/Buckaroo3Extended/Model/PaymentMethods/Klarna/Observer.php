a053fb4b1679d8c7e6af7819d4a4fbb0b4110968
Mickey Beijer <mickey.beijer@tig.nl>
2017-02-28 18:27:58 +0100

    Added basic setup for Reserve Soap call

---
+++ app/code/community/TIG/Buckaroo3Extended/Model/PaymentMethods/Klarna/Observer.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ *
+ *          ..::..
+ *     ..::::::::::::..
+ *   ::'''''':''::'''''::
+ *   ::..  ..:  :  ....::
+ *   ::::  :::  :  :   ::
+ *   ::::  :::  :  ''' ::
+ *   ::::..:::..::.....::
+ *     ''::::::::::::''
+ *          ''::''
+ *
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Creative Commons License.
+ * It is available through the world-wide-web at this URL:
+ * http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ * If you are unable to obtain it through the world-wide-web, please send an email
+ * to servicedesk@tig.nl so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future. If you wish to customize this module for your
+ * needs please contact servicedesk@tig.nl for more information.
+ *
+ * @copyright   Copyright (c) Total Internet Group B.V. https://tig.nl/copyright
+ * @license     http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ */
+class TIG_Buckaroo3Extended_Model_PaymentMethods_Klarna_Observer extends TIG_Buckaroo3Extended_Model_Observer_Abstract
+{
+    /** @var string $_code */
+    protected $_code = 'buckaroo3extended_klarna';
+
+    /** @var string $_method */
+    protected $_method = 'klarna';
+
+    /** @var  TIG_Buckaroo3Extended_Helper_Data $_helper */
+    protected $_helper;
+
+    /**
+     * Constructor to set helper
+     */
+    protected function _construct()
+    {
+        $this->_helper = Mage::helper('buckaroo3extended');
+    }
+
+    /**
+     * @param Varien_Event_Observer $observer
+     *
+     * @return $this
+     */
+    public function buckaroo3extended_request_setmethod(Varien_Event_Observer $observer)
+    {
+        if ($this->_isChosenMethod($observer) === false) {
+            return $this;
+        }
+
+        $request = $observer->getRequest();
+
+        $codeBits = explode('_', $this->_code);
+        $code = end($codeBits);
+
+        $request->setMethod($code);
+
+        return $this;
+    }
+
+    /**
+     * @param Varien_Event_Observer $observer
+     *
+     * @return $this
+     */
+    public function buckaroo3extended_request_addservices(Varien_Event_Observer $observer)
+    {
+        if ($this->_isChosenMethod($observer) === false) {
+            return $this;
+        }
+
+        $request = $observer->getRequest();
+        $vars = $request->getVars();
+
+        $array = array(
+            $this->_method => array(
+                'action'   => 'Reserve', //Authorize
+                'version'  => '1',
+            ),
+        );
+
+        if (array_key_exists('services', $vars) && is_array($vars['services'])) {
+            $vars['services'] = array_merge($vars['services'], $array);
+        } else {
+            $vars['services'] = $array;
+        }
+
+        $request->setVars($vars);
+
+        return $this;
+    }
+
+    /**
+     * @param Varien_Event_Observer $observer
+     *
+     * @return $this
+     */
+    public function buckaroo3extended_request_addcustomvars(Varien_Event_Observer $observer)
+    {
+        if ($this->_isChosenMethod($observer) === false) {
+            return $this;
+        }
+
+        $request            = $observer->getRequest();
+        $this->_billingInfo = $request->getBillingInfo();
+        $this->_order       = $request->getOrder();
+
+        $vars = $request->getVars();
+
+        //TODO: Add vars (soap data) for Reserve action
+
+        $request->setVars($vars);
+        return $this;
+    }
+}
