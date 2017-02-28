5e9665495e245879950387c14909885582960541
Mickey Beijer <mickey.beijer@tig.nl>
2017-02-28 15:26:29 +0100

    Added backend configuration fields

---
+++ app/code/community/TIG/Buckaroo3Extended/Model/Sources/Klarna/AvailableCurrencies.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ *
+ *          ..::..
+ *     ..::::::::::::..
+ *   ::'''''':''::'''''::
+ *   ::..  ..:  :  ....::
+ *   ::::  :::  :  :   ::
+ *   ::::  :::  :  ''' ::
+ *   ::::..:::..::.....::
+ *     ''::::::::::::''
+ *          ''::''
+ *
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Creative Commons License.
+ * It is available through the world-wide-web at this URL:
+ * http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ * If you are unable to obtain it through the world-wide-web, please send an email
+ * to servicedesk@tig.nl so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future. If you wish to customize this module for your
+ * needs please contact servicedesk@tig.nl for more information.
+ *
+ * @copyright   Copyright (c) Total Internet Group B.V. https://tig.nl/copyright
+ * @license     http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ */
+class TIG_Buckaroo3Extended_Model_Sources_Klarna_AvailableCurrencies
+{
+    /**
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        $paymentModel = Mage::getModel('buckaroo3extended/paymentMethods_klarna_paymentMethod');
+        $allowedCurrencies = $paymentModel->getAllowedCurrencies();
+
+        $array = array();
+        foreach ($allowedCurrencies as $allowedCurrency) {
+            $array[] = array('value' => $allowedCurrency, 'label' => $allowedCurrency);
+        }
+
+        return $array;
+    }
+}
