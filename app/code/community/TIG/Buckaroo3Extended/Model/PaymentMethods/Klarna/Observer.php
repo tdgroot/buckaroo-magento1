d42851ee996149a4557d4c3bcded36dd9ad6ca97
Mickey Beijer <mickey.beijer@tig.nl>
2017-03-06 15:42:52 +0100

    Added basic capture observer calls for pay webservice call

---
+++ app/code/community/TIG/Buckaroo3Extended/Model/PaymentMethods/Klarna/Observer.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ *
+ *          ..::..
+ *     ..::::::::::::..
+ *   ::'''''':''::'''''::
+ *   ::..  ..:  :  ....::
+ *   ::::  :::  :  :   ::
+ *   ::::  :::  :  ''' ::
+ *   ::::..:::..::.....::
+ *     ''::::::::::::''
+ *          ''::''
+ *
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Creative Commons License.
+ * It is available through the world-wide-web at this URL:
+ * http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ * If you are unable to obtain it through the world-wide-web, please send an email
+ * to servicedesk@tig.nl so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future. If you wish to customize this module for your
+ * needs please contact servicedesk@tig.nl for more information.
+ *
+ * @copyright   Copyright (c) Total Internet Group B.V. https://tig.nl/copyright
+ * @license     http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ */
+class TIG_Buckaroo3Extended_Model_PaymentMethods_Klarna_Observer extends TIG_Buckaroo3Extended_Model_Observer_Abstract
+{
+    /** @var string $_code */
+    protected $_code = 'buckaroo3extended_klarna';
+
+    /** @var string $_method */
+    protected $_method = 'klarna';
+
+    public function buckaroo3extended_capture_request_addservices(Varien_Event_Observer $observer)
+    {
+        if ($this->_isChosenMethod($observer) === false) {
+            return $this;
+        }
+
+        /** @var TIG_Buckaroo3Extended_Model_Request_Abstract $request */
+        $request = $observer->getRequest();
+        $vars = $request->getVars();
+
+        $array = array(
+            $this->_method => array(
+                'action'   => 'Pay', //Capture
+                'version'  => '1',
+            ),
+        );
+
+        if (array_key_exists('services', $vars) && is_array($vars['services'])) {
+            $vars['services'] = array_merge($vars['services'], $array);
+        } else {
+            $vars['services'] = $array;
+        }
+
+        $request->setVars($vars);
+
+        return $this;
+    }
+
+    public function buckaroo3extended_capture_request_addcustomvars(Varien_Event_Observer $observer)
+    {
+        if($this->_isChosenMethod($observer) === false) {
+            return $this;
+        }
+
+        /** @var TIG_Buckaroo3Extended_Model_Request_Abstract $request */
+        $request            = $observer->getRequest();
+        $this->_billingInfo = $request->getBillingInfo();
+        $this->_order       = $request->getOrder();
+
+        $vars = $request->getVars();
+
+        $vars['ReservationNumber'] = $this->_order->getBuckarooReservationNumber();
+
+        $request->setVars($vars);
+
+        return $this;
+    }
+}
