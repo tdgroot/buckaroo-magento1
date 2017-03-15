5406b7c2f5c9923400405ebc94d57d0319e97174
Mickey Beijer <mickey.beijer@tig.nl>
2017-03-15 17:34:16 +0100

    Added some Unittests for MPOS

---
+++ app/code/community/TIG/Buckaroo3Extended/Test/Unit/Model/PaymentMethods/Mpos/ObserverTest.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ *
+ *          ..::..
+ *     ..::::::::::::..
+ *   ::'''''':''::'''''::
+ *   ::..  ..:  :  ....::
+ *   ::::  :::  :  :   ::
+ *   ::::  :::  :  ''' ::
+ *   ::::..:::..::.....::
+ *     ''::::::::::::''
+ *          ''::''
+ *
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Creative Commons License.
+ * It is available through the world-wide-web at this URL:
+ * http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ * If you are unable to obtain it through the world-wide-web, please send an email
+ * to servicedesk@tig.nl so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future. If you wish to customize this module for your
+ * needs please contact servicedesk@tig.nl for more information.
+ *
+ * @copyright   Copyright (c) Total Internet Group B.V. https://tig.nl/copyright
+ * @license     http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ */
+class TIG_Buckaroo3Extended_Test_Unit_Model_PaymentMethods_Mpos_ObserverTest
+    extends TIG_Buckaroo3Extended_Test_Framework_TIG_Test_TestCase
+{
+    /** @var null|TIG_Buckaroo3Extended_Model_PaymentMethods_Mpos_Observer */
+    protected $_instance = null;
+
+    /**
+     * @return TIG_Buckaroo3Extended_Model_PaymentMethods_Mpos_Observer
+     */
+    protected function _getInstance()
+    {
+        if ($this->_instance === null) {
+            $this->_instance = new TIG_Buckaroo3Extended_Model_PaymentMethods_Mpos_Observer();
+        }
+
+        return $this->_instance;
+    }
+
+    /**
+     * @return PHPUnit_Framework_MockObject_MockObject|Mage_Sales_Model_Order
+     */
+    private function getMockOrder()
+    {
+//        $mockOrderAddress = $this->getMockBuilder('Mage_Sales_Model_Order_Address')->setMethods(null)->getMock();
+
+        $mockPayment = $this->getMockBuilder('Mage_Sales_Model_Order_Payment')
+            ->setMethods(array('getMethod'))
+            ->getMock();
+        $mockPayment->expects($this->any())->method('getMethod')->willReturn('buckaroo3extended_mpos');
+
+        $mockOrder = $this->getMockBuilder('Mage_Sales_Model_Order')
+            ->setMethods(array(
+                'getPayment',
+                'getPaymentMethodUsedForTransaction'
+//                'getBillingAddress',
+//                'getShippingAddress'
+            ))
+            ->getMock();
+
+        $mockOrder->expects($this->any())->method('getPayment')->will($this->returnValue($mockPayment));
+        $mockOrder->expects($this->any())->method('getPaymentMethodUsedForTransaction')->willReturn(false);
+//        $mockOrder->expects($this->any())->method('getBillingAddress')->willReturn($mockOrderAddress);
+//        $mockOrder->expects($this->any())->method('getShippingAddress')->willReturn($mockOrderAddress);
+
+        return $mockOrder;
+    }
+
+    /**
+     * @return PHPUnit_Framework_MockObject_MockObject|Varien_Event_Observer
+     */
+    private function getMockObserver()
+    {
+        $mockOrder = $this->getMockOrder();
+
+        $mockRequest = $this->getMockBuilder('TIG_Buckaroo3Extended_Model_Request_Abstract')
+            ->setMethods(array('getOrder'))
+            ->getMock();
+        $mockRequest->expects($this->any())->method('getOrder')->willReturn($mockOrder);
+
+        $mockObserver = $this->getMockBuilder('Varien_Event_Observer')
+            ->setMethods(array('getRequest', 'getOrder'))
+            ->getMock();
+        $mockObserver->expects($this->any())->method('getOrder')->willReturn($mockOrder);
+        $mockObserver->expects($this->any())->method('getRequest')->willReturn($mockRequest);
+
+        return $mockObserver;
+    }
+
+    public function testBuckaroo3extended_request_setmethod()
+    {
+        $mockObserver = $this->getMockObserver();
+
+        $instance = $this->_getInstance();
+        $result = $instance->buckaroo3extended_request_setmethod($mockObserver);
+        $requestMethodResult = $mockObserver->getRequest()->getMethod();
+
+        $this->assertInstanceOf('TIG_Buckaroo3Extended_Model_PaymentMethods_Mpos_Observer', $result);
+        $this->assertEquals('mpos', $requestMethodResult);
+    }
+}
