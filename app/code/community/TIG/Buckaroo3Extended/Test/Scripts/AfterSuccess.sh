9e1c6ae17f9ad1dbb01f42b2d8f6452f1e165f7e
Mickey Beijer <mickey.beijer@tig.nl>
2017-04-05 13:28:53 +0200

    fixed coveralls in AfterSuccess

--- app/code/community/TIG/Buckaroo3Extended/Test/Scripts/AfterSuccess.sh
+++ app/code/community/TIG/Buckaroo3Extended/Test/Scripts/AfterSuccess.sh
@@ -4,7 +4,9 @@ set -e
 set -x
 
 if [ "$CODE_COVERAGE" = "true" ]; then
-    sed -i -e "s|/tmp/magento/public/.modman/project/|${TRAVIS_BUILD_DIR}/|g" ${TRAVIS_BUILD_DIR}/build/logs/clover.xml
+    mv /tmp/magento/public/.modman/project/app/code/community/TIG/Buckaroo3Extended/Test/build/ ${TRAVIS_BUILD_DIR}
 
-    coveralls -vvv --config app/code/community/TIG/Buckaroo/Test/.coveralls.yml
+    sed -i -e "s|/tmp/magento/public/.modman/project/|${TRAVIS_BUILD_DIR}/|g" ${TRAVIS_BUILD_DIR}/build/logs/clover-*.xml
+
+    coveralls -vvv --config app/code/community/TIG/Buckaroo3Extended/Test/.coveralls.yml
 fi
