5e05e95de105b9011d9864146eb5e8df610cede2
Mickey Beijer <mickey.beijer@tig.nl>
2017-11-16 13:02:54 +0100

    BUCKM1-438 : Added unittests

--- app/code/community/TIG/Buckaroo3Extended/Test/Unit/Model/PaymentMethods/Klarna/PaymentMethodTest.php
+++ app/code/community/TIG/Buckaroo3Extended/Test/Unit/Model/PaymentMethods/Klarna/PaymentMethodTest.php
@@ -160,9 +160,66 @@ class TIG_Buckaroo3Extended_Test_Unit_Model_PaymentMethods_Klarna_PaymentMethodT
     {
         $mockPaymentInfo = $this->_getMockPayment();
         $instance = $this->_getInstance();
+        $instance->setData('info_instance', $mockPaymentInfo);
 
         $result = $instance->capture($mockPaymentInfo, 0);
 
         $this->assertInstanceOf('TIG_Buckaroo3Extended_Model_PaymentMethods_Klarna_PaymentMethod', $result);
+    }
+
+    /**
+     * @return array
+     */
+    public function canInvoicePartiallyProvider()
+    {
+        return array(
+            'full invoice, no discount'         => array(0, 1, 0, 1, true),
+            'partial invoice, no discount'      => array(0, 2, 0, 3, true),
+            'full invoice, order discount'      => array(0, 4, 5, 4, true),
+            'partial invoice, order discount'   => array(0, 5, 6, 7, false),
+            'full invoice, invoice discount'    => array(8, 9, 0, 9, true),
+            'partial invoice, invoice discount' => array(10, 11, 0, 12, false),
+            'full invoice, both discount'       => array(13, 14, 13, 14, true),
+            'partial invoice, both discount'    => array(15, 16, 15, 17, false),
+        );
+    }
+
+    /**
+     * @param $invoiceDiscount
+     * @param $invoiceTotal
+     * @param $orderDiscount
+     * @param $orderTotal
+     * @param $expected
+     *
+     * @dataProvider canInvoicePartiallyProvider
+     */
+    public function testCanInvoicePartially($invoiceDiscount, $invoiceTotal, $orderDiscount, $orderTotal, $expected)
+    {
+        $mockInvoice = $this->getMockBuilder(Mage_Sales_Model_Order_Invoice::class)
+            ->setMethods(array('getLastItem', 'getDiscountAmount', 'getBaseGrandTotal'))
+            ->getMock();
+        $mockInvoice->expects($this->once())->method('getLastItem')->willReturnSelf();
+        $mockInvoice->expects($this->once())->method('getDiscountAmount')->willReturn($invoiceDiscount);
+        $mockInvoice->expects($this->once())->method('getBaseGrandTotal')->willReturn($invoiceTotal);
+
+        $mockOrder = $this->getMockBuilder(Mage_Sales_Model_Order::class)
+            ->setMethods(array('getInvoiceCollection', 'getDiscountAmount', 'getBaseGrandTotal'))
+            ->getMock();
+        $mockOrder->expects($this->once())->method('getInvoiceCollection')->willReturn($mockInvoice);
+        $mockOrder->expects($this->once())->method('getDiscountAmount')->willReturn($orderDiscount);
+        $mockOrder->expects($this->once())->method('getBaseGrandTotal')->willReturn($orderTotal);
+
+        $mockPayment = $this->getMockBuilder(Mage_Sales_Model_Order_Payment::class)
+            ->setMethods(array('getOrder'))
+            ->getMock();
+        $mockPayment->expects($this->any())
+            ->method('getOrder')
+            ->willReturn($mockOrder);
+
+        $instance = $this->_getInstance();
+        $instance->setData('info_instance', $mockPayment);
+
+        $result = $instance->canInvoicePartially();
+        $this->assertEquals($expected, $result);
     }
 }
