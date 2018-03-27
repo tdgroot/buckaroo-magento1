d67199abde85c6c8c767a7c608e00430d5fe3a64
Mickey Beijer <mickey.beijer@tig.nl>
2018-03-27 16:04:13 +0200

    BUCKM1-469 : Afterpay ToS url is now dynamic depending on payment method and billing country

---
+++ app/code/community/TIG/Buckaroo3Extended/Test/Unit/Block/PaymentMethods/Afterpay/Checkout/FormTest.php
@@ -0,0 +1,142 @@
+<?php
+/**
+ *
+ *          ..::..
+ *     ..::::::::::::..
+ *   ::'''''':''::'''''::
+ *   ::..  ..:  :  ....::
+ *   ::::  :::  :  :   ::
+ *   ::::  :::  :  ''' ::
+ *   ::::..:::..::.....::
+ *     ''::::::::::::''
+ *          ''::''
+ *
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Creative Commons License.
+ * It is available through the world-wide-web at this URL:
+ * http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ * If you are unable to obtain it through the world-wide-web, please send an email
+ * to servicedesk@tig.nl so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future. If you wish to customize this module for your
+ * needs please contact servicedesk@tig.nl for more information.
+ *
+ * @copyright   Copyright (c) Total Internet Group B.V. https://tig.nl/copyright
+ * @license     http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ */
+class TIG_Buckaroo3Extended_Block_PaymentMethods_Afterpay_Checkout_FormTest extends TIG_Buckaroo3Extended_Test_Framework_TIG_Test_TestCase
+{
+    /** @var null|TIG_Buckaroo3Extended_Block_PaymentMethods_Afterpay_Checkout_Form */
+    private $_instance = null;
+
+    /** @var Mage_Sales_Model_Quote|PHPUnit_Framework_MockObject_MockObject */
+    private $quoteMock;
+
+    public function setUp()
+    {
+        $this->registerMockSessions('checkout');
+
+        $this->quoteMock = $this->getMockBuilder('Mage_Sales_Model_Quote')->disableOriginalConstructor()->getMock();
+
+        $checkoutSession = Mage::getSingleton('checkout/session');
+        $checkoutSession->method('getQuote')->willReturn($this->quoteMock);
+    }
+
+    /**
+     * @return TIG_Buckaroo3Extended_Block_PaymentMethods_Afterpay_Checkout_Form
+     */
+    protected function _getInstance()
+    {
+        if ($this->_instance === null) {
+            $this->_instance = new TIG_Buckaroo3Extended_Block_PaymentMethods_Afterpay_Checkout_Form();
+        }
+
+        return $this->_instance;
+    }
+
+    /**
+     * @return array
+     */
+    public function getB2CUrlProvider()
+    {
+        return array(
+            'BE' => array(
+                'BE',
+                'https://www.afterpay.be/be/footer/betalen-met-afterpay/betalingsvoorwaarden'
+            ),
+            'NL' => array(
+                'NL',
+                'https://www.afterpay.nl/nl/algemeen/betalen-met-afterpay/betalingsvoorwaarden'
+            ),
+            'DE' => array(
+                'DE',
+                'https://www.afterpay.nl/nl/algemeen/betalen-met-afterpay/betalingsvoorwaarden'
+            )
+        );
+    }
+
+    /**
+     * @param $country
+     * @param $expected
+     *
+     * @dataProvider getB2CUrlProvider
+     */
+    public function testGetB2CUrl($country, $expected)
+    {
+        $addressMock = $this->getMockBuilder('Mage_Customer_Model_Address')->setMethods(['getCountry'])->getMock();
+        $addressMock->expects($this->once())->method('getCountry')->willReturn($country);
+
+        $this->quoteMock->expects($this->atLeastOnce())->method('getBillingAddress')->willReturn($addressMock);
+
+        $instance = $this->_getInstance();
+        $result = $instance->getB2CUrl();
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * @return array
+     */
+    public function getB2BUrlProvider()
+    {
+        return array(
+            'BE' => array(
+                'BE',
+                'https://www.afterpay.nl/nl/algemeen/betalen-met-afterpay/betalingsvoorwaarden'
+            ),
+            'NL' => array(
+                'NL',
+                'https://www.afterpay.nl/nl/algemeen/zakelijke-partners/betalingsvoorwaarden-zakelijk'
+            )
+        );
+    }
+
+    /**
+     * @param $country
+     * @param $expected
+     *
+     * @dataProvider getB2BUrlProvider
+     */
+    public function testGetB2BUrl($country, $expected)
+    {
+        $addressMock = $this->getMockBuilder('Mage_Customer_Model_Address')->setMethods(['getCountry'])->getMock();
+        $addressMock->expects($this->once())->method('getCountry')->willReturn($country);
+
+        $this->quoteMock->expects($this->atLeastOnce())->method('getBillingAddress')->willReturn($addressMock);
+
+        $instance = $this->_getInstance();
+        $result = $instance->getB2BUrl();
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testGetAcceptgiroUrl()
+    {
+        $instance = $this->_getInstance();
+        $result = $this->invokeMethod($instance, 'getAcceptgiroUrl');
+        $this->assertEquals('https://www.afterpay.nl/nl/algemeen/betalen-met-afterpay/betalingsvoorwaarden', $result);
+    }
+}
